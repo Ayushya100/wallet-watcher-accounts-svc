bef50cf3f41355c40e8066a500374caad9a1c9b1
Ayushya <jaiswalayushya@gmail.com>
2024-03-17 15:48:52 +0530

    Initialized accounts service for wallet-watcher

---
+++ src/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import rateLimit from 'express-rate-limit';
+import { errorHandler } from 'lib-common-service';
+
+const app = express();
+
+// Setting up Middlewares
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, // reflecting the request origin
+    credentials: true
+}));
+
+app.use(express.json({
+    limit: '64kb' // Maximum request body size.
+}));
+
+app.use(express.urlencoded({
+    limit: '32kb',
+    extended: false
+}));
+
+app.use(rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes max
+    max: 50 // Limit each IP to 50 requests per windowMs
+}));
+
+app.use(express.static('public'));
+
+app.use(cookieParser());
+
+const tokenKey = process.env.ACCESS_TOKEN_KEY;
+
+// Error Handler middleware
+app.use(errorHandler);
+
+export default app;
