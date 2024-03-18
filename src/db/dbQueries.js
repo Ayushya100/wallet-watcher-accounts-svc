e203c4d860932fff0e34e301533a3009f7b1f32c
Ayushya <jaiswalayushya@gmail.com>
2024-03-18 10:23:32 +0530

    Updated createSetting API query parameters

--- src/db/dbQueries.js
+++ src/db/dbQueries.js
@@ -1,14 +1,14 @@
 'use strict';
 
 // Add DB Models
-import { DashboardSettingsModels } from 'lib-common-service';
+import { DashboardSettingsModel } from 'lib-common-service';
 
 const isSettingByNameAvailable = async(findParameter) => {
-    return await DashboardSettingsModels.findOne(findParameter);
+    return await DashboardSettingsModel.findOne(findParameter);
 }
 
 const registerNewSetting = async(createParameter) => {
-    return await DashboardSettingsModels.create(createParameter);
+    return await DashboardSettingsModel.create(createParameter);
 }
 
 export {
